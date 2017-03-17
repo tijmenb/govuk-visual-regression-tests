dcdfeab45e4b617a0c97e70b82e31b310acce79e
Tijmen Brommet <tijmen@gmail.com>
2017-03-17 07:57:33 +0000

    Use Docker

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ source 'https://rubygems.org'
 
 gem 'rake', '~> 12.0'
 gem 'http', '~> 2.2'
-gem 'wraith', '~> 4.0'
-gem 'nokogiri', '1.6.7'
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
 bundle exec rake generate_config
 
-bundle exec wraith capture generated-wraith-config.yml
+docker run -P -v ~/govuk/govuk-visual-regression-tests:/wraithy -w='/wraithy' bbcnews/wraith capture generated-wraith-config.yml
